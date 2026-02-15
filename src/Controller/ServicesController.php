94a5288521ec7825e830f661817f5b5ec2241d82
Machado <dbatistamachado1996@gmail.com>
2026-02-16 00:12:33 +0100

    Remove prices from services

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -16,31 +16,26 @@ class ServicesController extends AbstractController
                 'title' => 'Tonte de Pelouse & Entretien',
                 'description' => 'Tonte régulière, bordures et fertilisation pour garder votre pelouse luxuriante.',
                 'icon' => '🌱',
-                'price' => 'À partir de 50€'
             ],
             [
                 'title' => 'Création de Jardin Zen',
                 'description' => 'Conception et installation de jardins de rocaille paisibles, paysages de mousse et points d\'eau.',
                 'icon' => '🪨',
-                'price' => 'Devis Sur Mesure'
             ],
             [
                 'title' => 'Taille de Haies',
                 'description' => 'Taille de précision pour les haies et arbustes afin de maintenir forme et santé.',
                 'icon' => '✂️',
-                'price' => 'À partir de 80€'
             ],
             [
                 'title' => 'Plantation Saisonnière',
                 'description' => 'Sélection et plantation de fleurs et bulbes saisonniers pour de la couleur toute l\'année.',
                 'icon' => '🌻',
-                'price' => 'À partir de 100€'
             ],
             [
                 'title' => 'Nettoyage de Jardin',
                 'description' => 'Nettoyage complet de jardins envahis, y compris l\'enlèvement des déchets.',
                 'icon' => '🍂',
-                'price' => 'À partir de 200€'
             ],
         ];
 
